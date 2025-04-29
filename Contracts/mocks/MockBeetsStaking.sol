53cf3d1acd3b43334da6a74bf3291ddc7749476d
Emantesta <emantesta@gmail.com>
2025-04-29 14:28:46 +0100

    MockBeetsStaking.sol

--- Contracts/mocks/MockBeetsStaking.sol
+++ Contracts/mocks/MockBeetsStaking.sol
@@ -1,1 +1,16 @@
-
+contract MockBeetsStaking is IBeetsStaking {
+    mapping(address => uint256) public rewards;
+    function stake(address user, uint256 amount) external override {}
+    function withdraw(address user, uint256 amount) external override {}
+    function getReward(address user) external override returns (uint256) {
+        uint256 reward = rewards[user];
+        rewards[user] = 0;
+        return reward;
+    }
+    function balanceOf(address user) external view override returns (uint256) {
+        return 0;
+    }
+    function setReward(address user, uint256 amount) external {
+        rewards[user] = amount;
+    }
+}
