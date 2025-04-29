623917345a90619f40c96b6e5a3847f0eb89b921
Emantesta <emantesta@gmail.com>
2025-04-29 14:27:27 +0100

    MockGovernance.sol

--- Contracts/mocks/MockGovernance.sol
+++ Contracts/mocks/MockGovernance.sol
@@ -1,1 +1,11 @@
-
+contract MockGovernance is IGovernance {
+    uint256 public proposalCount;
+    function propose(bytes32 descriptionHash) external override returns (uint256) {
+        return ++proposalCount;
+    }
+    function vote(uint256 proposalId, bool support) external override {}
+    function execute(uint256 proposalId) external override {}
+    function getProposalState(uint256 proposalId) external view override returns (uint8) {
+        return 1; // Active
+    }
+}
