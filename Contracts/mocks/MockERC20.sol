688174f277eee088ccf954b57037d19598361523
Emantesta <emantesta@gmail.com>
2025-04-29 14:50:49 +0100

    MockERC20.sol

--- Contracts/mocks/MockERC20.sol
+++ Contracts/mocks/MockERC20.sol
@@ -1,1 +1,12 @@
+// Mock Contracts for Testing. Create mock contracts in AfroVibe/contracts/mocks/ to simulate external interfaces.
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
