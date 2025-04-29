35a6dc4963febc87b66652063630d4c30ab930a2
Emantesta <emantesta@gmail.com>
2025-04-29 15:28:10 +0100

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,16 @@
 // Hardhat config
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sonic: {
+      url: process.env.SONIC_RPC_URL || "https://rpc.sonic.network",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    hardhat: {
+      chainId: 1337
+    }
+  }
+};
