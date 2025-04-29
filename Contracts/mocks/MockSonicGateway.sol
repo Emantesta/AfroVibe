380c64e095ba5ccad5bc0e2df511d69a358e5912
Emantesta <emantesta@gmail.com>
2025-04-29 14:26:43 +0100

    MockSonicGateway.sol

--- Contracts/mocks/MockSonicGateway.sol
+++ Contracts/mocks/MockSonicGateway.sol
@@ -1,1 +1,8 @@
-
+contract MockSonicGateway is ISonicGateway {
+    function bridge(address token, uint256 amount, address recipient, bool toEthereum) external override {}
+    function receiveTokens(address token, uint256 amount, address recipient) external override {}
+    function setBridgeLimits(uint256 maxAmount, uint256 userLimit) external override {}
+    function isBridgingPaused() external view override returns (bool) {
+        return false;
+    }
+}
