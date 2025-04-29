35034aed0a79fddba24707113d882bd075502b99
Emantesta <emantesta@gmail.com>
2025-04-29 15:55:33 +0100

    mapping.ts

--- backend/src/mapping.ts
+++ backend/src/mapping.ts
@@ -1,1 +1,9 @@
 // Event handlers
+
+export function handleStaked(event: Staked): void {
+  let stake = new Stake(event.transaction.hash.toHex());
+  stake.user = event.params.user;
+  stake.amount = event.params.amount;
+  stake.lockPeriod = event.params.lockPeriod;
+  stake.save();
+}
