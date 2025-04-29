f29e2ab268fb8913863f92a5531ed580e5869612
Emantesta <emantesta@gmail.com>
2025-04-29 14:25:28 +0100

    MockSonicValidator.sol

--- Contracts/mocks/MockSonicValidator.sol
+++ Contracts/mocks/MockSonicValidator.sol
@@ -1,1 +1,10 @@
-
+contract MockSonicValidator is ISonicValidator {
+    function delegate(address user, uint256 amount) external override {}
+    function undelegate(address user, uint256 amount) external override {}
+    function getValidatorInfo(address validator) external view override returns (uint256 stake, uint256 rewards) {
+        return (0, 0);
+    }
+    function maxDelegation() external view override returns (uint256) {
+        return 1_000_000 ether;
+    }
+}
