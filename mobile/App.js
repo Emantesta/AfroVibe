222e5b1870604a71c080e408ad1afd5f2f8d41c7
Emantesta <emantesta@gmail.com>
2025-04-30 14:51:40 +0100

    App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,1 +1,7 @@
-//  Mobile entry
+//  Mobile entry; initialize firebase in app.js
+import analytics from '@react-native-firebase/analytics';
+
+async function initializeAnalytics() {
+  await analytics().setAnalyticsCollectionEnabled(true);
+}
+initializeAnalytics();
