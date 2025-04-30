8bfd42772630b6ad43954ce7b5b63823fb677a9e
Emantesta <emantesta@gmail.com>
2025-04-30 16:55:37 +0100

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,17 +2,11 @@ async function main() {
   const Staking = await ethers.getContractFactory("Staking");
   const staking = await upgrades.deployProxy(Staking, [
     process.env.S_TOKEN_ADDRESS,
-    process.env.
-    process.env.
-    process.env.
-    process.env.
-    process.env.
-    process.env.
-    process.env.
-    process.env.
-    process.env.
-    process.env.
-    process.env.
+    process.env.USDC_ADDRESS,
+    process.env.DEX_ROUTER_ADDRESS,
+    process.env.STAKING_CONTRACT_ADDRESS,
+    process.env.SIMPLE_ACCOUNT_FACTORY_ADDRESS,
+    process.env.ENTRY_POINT_ADDRESS,
     process.env.BEETS_STAKING_ADDRESS,
     process.env.GOVERNANCE_ADDRESS,
     process.env.PLATFORM_DAO_ADDRESS,
