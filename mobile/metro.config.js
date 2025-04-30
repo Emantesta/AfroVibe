7a3bc4892a30f3559e3773ac62b7ae9ba5e4ee5a
Emantesta <emantesta@gmail.com>
2025-04-30 15:12:39 +0100

    metro.config.js

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,1 +1,23 @@
+const { getDefaultConfig } = require('metro-config');
 
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-dotenv'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      sourceExts: [...sourceExts, 'js', 'json', 'ts', 'tsx'],
+      assetExts: [...assetExts, 'ttf', 'otf'], // Support font files for Inter font
+    },
+  };
+})();
