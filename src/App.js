e07f882461eab487a4a9a2b0841ceddc8229cd6a
Emantesta <emantesta@gmail.com>
2025-04-30 15:03:45 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 // Initialize firebase in App.js
 import analytics from '@react-native-firebase/analytics';
+import { useFonts, Inter_400Regular, Inter_700Bold } from '@react-native-google-fonts/inter';
+
+const App = () => {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
+  if (!fontsLoaded) return null;
+  return <AnalyticsDashboard />;
+};
 
 async function initializeAnalytics() {
   await analytics().setAnalyticsCollectionEnabled(true);
