cb31f8ddca952e6fd63afef8e37efb6427e9ca41
Emantesta <emantesta@gmail.com>
2025-04-30 16:16:48 +0100

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,15 @@
-
+async function main() {
+  const Staking = await ethers.getContractFactory("Staking");
+  const staking = await upgrades.deployProxy(Staking, [
+    process.env.S_TOKEN_ADDRESS,
+    process.env.BEETS_STAKING_ADDRESS,
+    process.env.GOVERNANCE_ADDRESS,
+    process.env.PLATFORM_DAO_ADDRESS,
+    process.env.PAYMASTER_ADDRESS,
+    process.env.TIMELOCK_ADDRESS,
+    process.env.SONIC_GATEWAY_ADDRESS,
+    process.env.SONIC_VALIDATOR_ADDRESS,
+    process.env.FEE_RECIPIENT_ADDRESS
+  ], { initializer: "initialize" });
+  console.log("Staking deployed to:", staking.address);
+}
