4427eccfa6d7e6eb39e132560506c117bad7db52
Emantesta <emantesta@gmail.com>
2025-05-01 15:49:46 +0100

    config.js

--- streaming-server/config.js
+++ streaming-server/config.js
@@ -1,1 +1,19 @@
 // Streaming config
+module.exports = {
+  port: 8080,
+  maxBitrate: '800k',
+  eventStream: {
+    arEnabled: true,
+    latency: 'low',
+    maxUsers: 10000,
+    codecs: ['H264', 'VP8']
+  },
+  liveStream: {
+    maxBitrate: '600k',
+    codecs: ['H264']
+  },
+  security: {
+    cors: ['https://afrovibe.io'],
+    rateLimit: 100
+  }
+};
