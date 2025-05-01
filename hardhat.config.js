b9ce04408616a7e220a38c63d704984152505cbd
Emantesta <emantesta@gmail.com>
2025-05-01 15:58:52 +0100

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 // Hardhat config
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
