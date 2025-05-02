14b12e64dfbbf0f3c8a383272ed0fb8e8ef1b921
Emantesta <emantesta@gmail.com>
2025-05-02 10:26:03 +0100

    Reward.sol

--- Contracts/Reward.sol
+++ Contracts/Reward.sol
@@ -1,1 +1,1 @@
-// Rewards for subscriptions, GameFi, music and governance voting; Engagement/curator rewards
+// Rewards for subscriptions, GameFi, music and governance voting; Engagement/curator rewards; **Curation**: USDC bounties for trending posts (`Reward.sol`).
