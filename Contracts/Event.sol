85fe27b61e4570571d81ec2af8f3abd894e0efa4
Emantesta <emantesta@gmail.com>
2025-05-02 14:04:34 +0100

    Event.sol

--- Contracts/Event.sol
+++ Contracts/Event.sol
@@ -1,1 +1,38 @@
 // Virtual events and ticket sales; 30-90% event ticket sales
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "./NFT.sol";
+
+contract Event {
+    IERC20 public usdc;
+    NFT public nft;
+    struct VirtualEvent {
+        address organizer;
+        uint256 ticketPrice;
+        uint256 totalTickets;
+        uint256 sold;
+    }
+    mapping(uint256 => VirtualEvent) public events;
+
+    event TicketPurchased(uint256 eventId, address buyer, uint256 nftId);
+
+    constructor(address _usdc, address _nft) {
+        usdc = IERC20(_usdc);
+        nft = NFT(_nft);
+    }
+
+    function createEvent(uint256 eventId, uint256 ticketPrice, uint256 totalTickets) external {
+        events[eventId] = VirtualEvent(msg.sender, ticketPrice, totalTickets, 0);
+    }
+
+    function buyTicket(uint256 eventId) external {
+        VirtualEvent storage evt = events[eventId];
+        require(evt.sold < evt.totalTickets, "Sold out");
+        usdc.transferFrom(msg.sender, address(this), evt.ticketPrice);
+        usdc.transfer(evt.organizer, evt.ticketPrice * 90 / 100); // 10% fee
+        evt.sold++;
+        uint256 nftId = nft.mint(msg.sender, "Event_NFT");
+        emit TicketPurchased(eventId, msg.sender, nftId);
+    }
+}
