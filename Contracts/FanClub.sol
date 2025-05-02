028c9eb391cbfc96f62cd9407a8487cf7b177edd
Emantesta <emantesta@gmail.com>
2025-05-02 14:20:52 +0100

    FanClub.sol

--- Contracts/FanClub.sol
+++ Contracts/FanClub.sol
@@ -1,1 +1,38 @@
 // AI-driven fan clubs; 30-90% fan club fees
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "./NFT.sol";
+
+contract FanClub {
+    IERC20 public usdc;
+    NFT public nft;
+    struct Club {
+        address creator;
+        uint256 subscriptionFee;
+        uint256 memberCount;
+    }
+    mapping(uint256 => Club) public clubs;
+
+    event JoinedClub(uint256 clubId, address fan, uint256 fee);
+
+    constructor(address _usdc, address _nft) {
+        usdc = IERC20(_usdc);
+        nft = NFT(_nft);
+    }
+
+    function createClub(uint256 clubId, uint256 subscriptionFee) external {
+        clubs[clubId] = Club(msg.sender, subscriptionFee, 0);
+    }
+
+    function joinClub(uint256 clubId) external {
+        Club storage club = clubs[clubId];
+        usdc.transferFrom(msg.sender, address(this), club.subscriptionFee);
+        usdc.transfer(club.creator, club.subscriptionFee * 80 / 100); // 20% fee
+        club.memberCount++;
+        if (club.memberCount % 100 == 0) { // Superfan every 100th member
+            nft.mint(msg.sender, "Superfan_NFT");
+        }
+        emit JoinedClub(clubId, msg.sender, club.subscriptionFee);
+    }
+}
