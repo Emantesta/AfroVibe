5966b54be6f976ab481067ddb7ade8037c99fe01
Emantesta <emantesta@gmail.com>
2025-05-03 19:53:24 +0100

    MusicStreaming.sol

--- Contracts/MusicStreaming.sol
+++ Contracts/MusicStreaming.sol
@@ -1,1 +1,2 @@
 // Decentralized music streaming, NFT releases; 25-90% streaming revenue
+// Artists upload tracks, earn USDC per stream, and release NFT-based limited editions, celebrating Nigeria’s Afrobeats culture.
