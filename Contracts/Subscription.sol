7fc55b2b95dfbe45b15c97d01a694035beddd61a
Emantesta <emantesta@gmail.com>
2025-05-03 19:51:36 +0100

    Subscription.sol

--- Contracts/Subscription.sol
+++ Contracts/Subscription.sol
@@ -1,1 +1,2 @@
 // Creator subscriptions for premium content; USDC subscriptions, smart wallets; 30-90% subscription fees
+// Monetize exclusive content via tiered subscriptions, including private posts, live streams, and personalized interactions, paid in USDC.
