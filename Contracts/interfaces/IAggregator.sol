904028cd5e3986ed720fbec796a47cd361e174f7
Emantesta <emantesta@gmail.com>
2025-05-06 13:08:06 +0100

    IAggregator.sol

---
+++ Contracts/interfaces/IAggregator.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+import "./UserOperation.sol";
+
+interface IAggregator {
+    function validateUserOpSignature(UserOperation calldata userOp) external view returns (bytes memory sigForUserOp);
+    function aggregateSignatures(UserOperation[] calldata userOps) external view returns (bytes memory aggregatesSignature);
+    function validateSignatures(UserOperation[] calldata userOps, bytes calldata signature) external view;
+}
